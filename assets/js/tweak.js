c00270ad9b9299f274481801680a680ef000ec9c
Ghifani <ghifaniikhsan114@gmail.com>
2023-09-17 11:43:51 +0700

    Membuat sebuah humburger-menu

---
+++ assets/js/tweak.js
@@ -0,0 +1,10 @@
+let humburger_menu = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+humburger_menu.addEventListener("click", function() {
+    navbar.classList.toggle("active")
+})
+
+window.onscroll = () => {
+    navbar.classList.remove("active")
+}
