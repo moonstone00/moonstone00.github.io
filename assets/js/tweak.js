fe23d8012934edd7357b8612d50ecb7603ffff4b
Ghifani <ghifaniikhsan114@gmail.com>
2023-09-21 17:34:02 +0700

    Menambahkan script js

--- assets/js/tweak.js
+++ assets/js/tweak.js
@@ -8,3 +8,24 @@ humburger_menu.addEventListener("click", function() {
 window.onscroll = () => {
     navbar.classList.remove("active")
 }
+
+function sendMail() {
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    }
+    const serviceID = "service_1midykm"
+    const templateID = "template_j81tstj"
+    
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = ""
+            document.getElementById("email").value = ""
+            document.getElementById("message").value = ""
+            console.log(res)
+            alert("Your message sent successfully")
+        })
+        .catch((err) => console.log(err))
+}
